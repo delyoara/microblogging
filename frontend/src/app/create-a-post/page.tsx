eddde6123e0370d6bfec3c868058fbf9db6548db
rerefenec <renaud.delorme@free.fr>
2025-07-21 17:13:20 +0200

    first commit

---
+++ frontend/src/app/create-a-post/page.tsx
@@ -0,0 +1,23 @@
+"use client"; 
+
+import Header from "@/components/Header";
+import { Container } from "lucide";
+
+
+
+export default function post() {
+  return (
+    <main className="relative min-h-screen">
+      <Header />
+<body>
+<div className="px-20 py-20 bg-green-200 text-center">
+create a post
+</div>
+
+</body>
+
+    </main>
+
+    
+  );
+}
