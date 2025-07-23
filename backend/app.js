ce705f5e8635a0ec4fe9f7520a9e5f9a07dd5de2
danisaza1 <dani_isaza@htomail.com>
2025-07-23 14:03:44 +0200

    corriger fautes d'ortographe

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,29 @@
-
 import express from 'express';
 import cors from 'cors';
 import postsRoutes from './routes/posts.js';
 import usersRoutes from './routes/users.js';
-import commentRoutes from './routes/comment.js';
+import commentRoutes from './routes/comments.js';
+import themeRoutes from './routes/themes.js';
 
 const app = express();
 const port = 3001;
 
 app.use(express.json());
-app.use(cors());
+
+// CORS : autorise uniquement ton frontend local
+app.use(cors({
+  origin: 'http://localhost:3000',
+}));
 
 app.use('/api/posts', postsRoutes);
 app.use('/api/users', usersRoutes);
-app.use('api/comments', commentRoutes);
-app.use('api/themes', themeRoutes);
-
+app.use('/api/comments', commentRoutes);
+app.use('/api/themes', themeRoutes);
 
 app.get('/', (req, res) => {
-  res.send('La vie est belle!')
+  res.send('La vie est belle!');
 });
 
 app.listen(port, () => {
-  console.log(`🚀 Serveur lancé sur : http://localhost:${port}`)
+  console.log(`🚀 Serveur lancé sur : http://localhost:${port}`);
 });
